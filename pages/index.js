5ae278f1326fa65bdac518fab81a1ae6f5f22a3d
Emanuele <celestinoemanuele015@gmail.com>
2022-12-12 13:58:17 +0100

    s

--- pages/index.js
+++ pages/index.js
@@ -219,7 +219,7 @@ export default function Home() {
 
               <Grid container columns={{ xs: 100, sm: 100, md: 100 }} style={{paddingLeft: '2vw', paddingRight: '2vw', display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap'}}>
 
-                {allAnime.slice(-4).reverse().map((_, index) => (
+                {allAnime.slice(-4).map((_, index) => (
                   <Grid item key={index} style={{maxWidth: '400px'}}>
                     <CardComponent Nome={_.Nome} Uscita={_.Uscita} Stato={_.Stato} Copertina={_.Copertina} Id={_._id} Trama={_.Trama} Generi={_.Generi}/>
                   </Grid>
