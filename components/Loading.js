c63473bdd11c4569cd956b626b0d3b924a4b1a52
Emanuele <celestinoemanuele015@gmail.com>
2022-12-13 13:54:19 +0100

    h

--- components/Loading.js
+++ components/Loading.js
@@ -1,13 +1,42 @@
-import Image from 'next/image'
-import loadingGIF from '/public/loadingGIF.gif';
-import favicon from "/public/favicon.ico";
+
+ 
 
 function Loading(props) {
     return (
-        <div style={{width: '-webkit-fill-available', flexDirection: 'column-reverse', position: 'absolute', top: '0px', left: '0', justifyContent: 'center', alignItems: 'center', backgroundColor: 'black', height: '100vh', zIndex: '9999', display: 'flex', flexWrap: 'nowrap'}}>
-            <Image className="heartbeat" src={loadingGIF} style={{width: '100px', height: 'auto', borderRadius: '30px'}}/> 
-            <br></br>
-        </div>
+        <>
+            <div style={{width: '-webkit-fill-available', flexDirection: 'column', position: 'absolute', top: '0px', left: '0', justifyContent: 'center', alignItems: 'center', backgroundColor: 'black', height: '100vh', zIndex: '9999', display: 'flex', flexWrap: 'nowrap'}}>
+                <img className="heartbeat" src="/loadingGIF.gif" style={{width: '100px', height: 'auto', borderRadius: '30px'}}/> 
+                
+                <br></br>
+                <img src='/eventoNatale/slitta.png' className="slitta" style={{width: '80px', height: 'auto', }}/>
+                <p>BUONE FESTE DAL TEAM DI ANIMECROWD ❤️</p>
+            </div>
+                
+                <style>
+                    {`
+                        .slitta {
+
+                            -moz-animation: slittaanim 4s infinite;
+                            -webkit-animation: slittaanim 4s infinite;
+                                -o-animation: slittaanim 4s infinite;
+                                    animation: slittaanim 4s infinite;
+                            
+                            margin-right: auto;
+                            margin-left: -200px;
+                            position: relative;
+                            z-index: 444444;
+
+                            display: block;
+                            
+                            width: 50%;
+                        }
+                        @keyframes slittaanim {
+                            from {left: 0vw}
+                            to {left: 150vw}
+                        }
+                    `}
+                </style>
+        </>
     )
 }
 
