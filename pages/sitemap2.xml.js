32a3fca94dd3ae6657ff10ead5fa6d45e2a7fa45
Emanuele <celestinoemanuele015@gmail.com>
2022-12-30 15:55:57 +0100

    s

--- pages/sitemap2.xml.js
+++ pages/sitemap2.xml.js
@@ -11,11 +11,6 @@ let currentDate = `${year}-${month}-${day}`;
 function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-            <loc>https://www.animecrowd.it/</loc>
-            <lastmod>${`${currentDate}`}</lastmod>
-            <changefreq>always</changefreq>
-        </url>
         ${posts
             .map(({ _id }) => {
                 return `
