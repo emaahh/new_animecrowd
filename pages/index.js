8e014a84a235752ea181b13ab41e000989c504e7
Emanuele <celestinoemanuele015@gmail.com>
2023-01-11 10:24:24 +0100

    ads

--- pages/index.js
+++ pages/index.js
@@ -297,12 +297,24 @@ export default function Home() {
       <br></br>
 
 
-      <Script id={unique_id} async="async" data-cfasync="false" src={`//pl17727417.highperformancecpmgate.com/65b96abcfdde95022fd25ce3998d9433/invoke.js?v=${unique_id}`}></Script>
-      <div id="container-65b96abcfdde95022fd25ce3998d9433"></div>
-      <Script id={unique_id} async="async" data-cfasync="false" src={`//pl18252550.highcpmrevenuenetwork.com/9dea030dc93e767553bb9c0061cc5940/invoke.js?v=${unique_id}`}></Script>
-      <div id="container-9dea030dc93e767553bb9c0061cc5940"></div>
-      <Script id={unique_id} async="async" data-cfasync="false" src={`//pl18252549.highcpmrevenuenetwork.com/6f3cae702227cd5f51b5ce0f8350e6c0/invoke.js?v=${unique_id}`}></Script>
-      <div id="container-6f3cae702227cd5f51b5ce0f8350e6c0"></div>
+      <div id="ts_ad_native_rackl"></div>
+      <Script
+        src="//cdn.runative-syndicate.com/sdk/v1/n.js"
+        onLoad={() => {
+          NativeAd({
+            element_id: "ts_ad_native_rackl",
+            spot: "c939fab80dfa473187afa2b40b0c48cc",
+            type: "label-under",
+            cols: 4,
+            rows: 1,
+            mobileEnabled: false,
+            title: "Suggested for you",
+            titlePosition: "left",
+            adsByPosition: "right",
+          });
+        }}
+      />
+
     
     </div>
   )
