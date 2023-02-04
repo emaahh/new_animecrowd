cc1b38ca6db933f33c255e3eeef21dc54b6098c9
Emanuele <celestinoemanuele015@gmail.com>
2023-02-04 19:43:56 +0100

    s

--- pages/index.js
+++ pages/index.js
@@ -301,7 +301,7 @@ export default function Home() {
       <br></br>
 
       <Script id="show-banner">
-        {`    <script>(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('osspalkiaom.com',5697204,document.createElement('script'))</script>`}
+        {`(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('osspalkiaom.com',5697204,document.createElement('script'))`}
       </Script>
       
 
