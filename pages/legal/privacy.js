bf60d4b5f8bd54700926f2f7299eaeeb837e5f3b
Emanuele <celestinoemanuele015@gmail.com>
2023-03-05 23:10:28 +0100

    ss

--- pages/legal/privacy.js
+++ pages/legal/privacy.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState, useMemo } from "react";
+import React, { useEffect, useRef, useState, useMemo } from  "react" ;
 const Privacy = () => {
-  return <div style={{paddingTop:  "90px" }}>
+  return <div style={{paddingTop:  "90px"  }}>
 
 <h1>Privacy Policy for AnimeCrowd</h1>
 
@@ -44,7 +44,7 @@ const Privacy = () => {
 
 <h2>Google DoubleClick DART Cookie</h2>
 
-<p>Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – <a href="https://policies.google.com/technologies/ads">https://policies.google.com/technologies/ads</a></p>
+<p>Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – <a href="https://policies.google.com/technologies/ads" >https://policies.google.com/technologies/ads</a></p>
 
 <h2>Our Advertising Partners</h2>
 
